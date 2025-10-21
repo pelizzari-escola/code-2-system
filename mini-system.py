995e851ce4eff5d4083aeadeab83cc4794586223
Professor Vinícius <vinicius.pelizzari.silva@escola.pr.gov.br>
2025-10-21 08:01:05 -0300

    Add files via upload

--- mini-system.py
+++ mini-system.py
@@ -10,12 +10,21 @@ def cadastro_new_user():
         new_password = input("Tente novamente! Digite sua nova senha: ")
     print("Olá, ", new_username, ". Você foi cadastrado!")
 
-opcao_inicial = int(input("Digite 1 para fazer LOGIN ou Digite 2 para CADASTRO: \n"))
+opicao_inical = int(input("Digite 1 para fazer LOGIN ou Digite 2 para CADASTRO: \n"))
 if opcao_inicial == 1 or opcao_inicial == 2:
     if opcao_inicial == 1:
+        nome_cad = "vini"
+        senha_cad = "123"
+
         username_cad = input("Digite seu usuário cadastrado: ")
         password_cad = input("Digite sua senha cadastrada: ")
-        print("Usuário Logado no MINI SYSTEM")
+
+        if username_cad == nome_cad:
+            while password_cad != senha_cad:
+                password_cad = input("Tente novamente! Digite sua senha cadastrada: ")
+            print("Usuário Logado no MINI SYSTEM")
+        else:
+            print("Usuário não cadastrado!")
     else:
         cadastro_new_user()
 else:
